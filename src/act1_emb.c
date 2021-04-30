dd99d3a5d54a9ab764c3c6aad6b15adf3fa21708
Prathzz <80695458+Prathzz@users.noreply.github.com>
2021-04-30 22:25:39 +0530

    Add files via upload

---
+++ src/act1_emb.c
@@ -0,0 +1,22 @@
+/**
+ * @file act1_emb.c
+ * @author Prathamesh Devadiga
+ * @version 0.1
+ * @date 2021-04-25
+ * @copyright Copyright (c) 2021
+ */
+
+#include<avr/io.h>
+#include "act_1_emb.h"
+
+void state1(uint8_t state)
+{
+    LED_PORT = (state << LED_PIN);
+}
+void LED(void)
+{
+    DDRB = DDRB | (1<<PB0);
+    DDRD = DDRD & (~(1<<PD0));
+    PORTD = PORTD | (1<<PD0);
+    PORTD = PORTD | (1<<PD1);
+}
---
+++ src/act2_emb.c
@@ -0,0 +1,31 @@
+/**
+ * @file act2_emb.c
+ * @author Prathamesh Devadiga
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<avr/io.h>
+#include "act_2_emb.h"
+
+uint16_t Read_ADC(uint8_t ch)
+{
+    ADMUX = ADMUX & 0xf8;
+    ch=ch&0b00000111;
+    ADMUX = ADMUX | ch;
+    ADCSRA = ADCSRA | (1<<ADSC);
+    
+    while(!(ADCSRA & (1<<ADIF)));
+
+    ADCSRA = ADCSRA | (1<<ADIF);
+    return(ADC);
+}
+
+void Initilize_ADC()
+{
+    ADMUX=(1<<REFS0);
+    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+}
