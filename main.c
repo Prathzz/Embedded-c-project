68a0cc78b4ed682bdb53e209a8ccf17c6262cc98
Prathzz <80695458+Prathzz@users.noreply.github.com>
2021-04-30 22:27:54 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,58 @@
+/**
+ * @file Main.c
+ * @author Raghavendra A M
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "act_1_emb.h"
+#include "act_2_emb.h"
+#include "act_3_emb.h"
+#include "act_4_emb.h"
+#include<avr/io.h>
+void peripheral_init(void)
+{
+    
+    LED(); //Configure LED and Switch pins
+    
+    Initilize_ADC(); //Configure ADC registers and pins
+   
+    Initialize_PWM(); //Configure PWM registers and pins
+    
+    Initialize_UART(103); //Configure UART serial communication pin
+}
+    
+   
+uint16_t temp;
+char temp_data;
+int main(void)
+{
+    // Initialize peripherals
+    peripheral_init();
+    while(1)
+    {
+        if(SENSOR_ON) //If switch_1 is ON
+        {
+            if(HEAT_ON) //If switch_2 is ON
+            {
+                state1(LED_ON);//LED is ON
+                temp=Read_ADC(0);
+                temp_data = Output_PWM(temp);
+                UART_write(temp_data);
+            }
+            else
+            {  
+                state1(LED_OFF);
+            }
+        }
+        else
+        {
+            state1(LED_OFF);//LED is OFF
+            OCR1A=0;
+        }
+    }
+    return 0;
+}
